17640a4edfad6d9e1e1bc03f89e724ac48b014f1
ryok13 <mario24ryok@gmail.com>
2025-08-12 12:14:17 -0400

    Initial commit

---
+++ src/main/java/com/georgiancollege/finalexam_200605831/HelloController.java
@@ -0,0 +1,14 @@
+package com.georgiancollege.finalexam_200605831;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.georgiancollege.finalexam_200605831 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.georgiancollege.finalexam_200605831 to javafx.fxml;
+    exports com.georgiancollege.finalexam_200605831;
+}
