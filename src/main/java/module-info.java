76969563e27de42d44c34a4afdf3eee85a412f0e
ryok13 <mario24ryok@gmail.com>
2025-08-12 13:33:42 -0400

    Update files for gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.georgiancollege.finalexam_200605831 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires java.desktop;
+    requires com.google.gson;
 
 
-    opens com.georgiancollege.finalexam_200605831 to javafx.fxml;
+    opens com.georgiancollege.finalexam_200605831 to javafx.fxml, com.google.gson;
     exports com.georgiancollege.finalexam_200605831;
 }
