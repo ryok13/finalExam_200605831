1594a9403b2dfc3baab10177c0e0847e8128a34e
ryok13 <mario24ryok@gmail.com>
2025-08-12 13:58:57 -0400

    Question 3 complete

---
+++ src/main/java/com/georgiancollege/finalexam_200605831/JsonReader.java
@@ -0,0 +1,27 @@
+package com.georgiancollege.finalexam_200605831;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class JsonReader {
+    private static final Gson gson = new Gson();
+    public static List<Customer> loadCustomers() throws IOException {
+        try (InputStream is = JsonReader.class.getResourceAsStream("/customers.json")) {
+            if (is == null) throw new FileNotFoundException("customers.json not found");
+            try (Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+                JsonObject root = JsonParser.parseReader(r).getAsJsonObject();
+                JsonArray customersArray = root.getAsJsonArray("Customers");
+                Type listType = new TypeToken<List<Customer>>() {}.getType();
+                return new Gson().fromJson(customersArray, listType);
+            }
+        }
+    }
+}
